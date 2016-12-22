c59a3deaa129a0d381b81870caf9d17da18be5ee
Jared Kells <jkells@gmail.com>
2016-12-22 20:37:53 +1100

    WIP

---
+++ src/Exporters/IExporter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using PocketBookSync.Data;
+
+namespace PocketBookSync.Exporters
+{
+    public interface IExporter : IDisposable
+    {
+        Task<IEnumerable<Transaction>> ExportRecent();
+    }
+}
